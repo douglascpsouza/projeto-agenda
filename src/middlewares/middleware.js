9ec55f9ae2d6a6d0bcd96a3e1b2228dddb605623
Douglas Souza <douglascpsouza@gmail.com>
2022-09-25 22:04:05 -0300

    Set two error pages for the checkCsrfError middleware

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,17 +1,20 @@
 // Global Middleware
 
 exports.globalMiddleware = (req, res, next) => {
-  res.locals.testVariable = 'Middleware test...'
+  res.locals.testVariable = 'Middleware test...';
   next();
-}
+};
 
 exports.checkCsrfError = (err, req, res, next) => {
   if (err && err.code === 'EBADCSRFTOKEN') {
     return res.render('403');
+  } else if (err) {
+    return res.render('404');
   }
-}
+  next();
+};
 
 exports.csrfMiddleware = (req, res, next) => {
   res.locals.csrfToken = req.csrfToken();
   next();
-}
+};
