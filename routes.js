58e840714dccdb9ac077de421423738c73f25cf7
Douglas Souza <douglascpsouza@gmail.com>
2022-09-26 15:08:39 -0300

    Add logout route and controller

--- routes.js
+++ routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const route = express.Router();
 const homeController = require('./src/controllers/homeController');
 const loginController = require('./src/controllers/loginController');
+const logoutController = require('./src/controllers/logoutController');
 const registerController = require('./src/controllers/registerController');
 
 // Home route
@@ -9,6 +10,10 @@ route.get('/', homeController.index);
 
 // Login routes
 route.get('/login', loginController.index);
+route.post('/login', loginController.login);
+
+// Logout routes
+route.get('/logout', logoutController.index);
 
 // Register routes
 route.get('/register', registerController.index);
